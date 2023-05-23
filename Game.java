b716006976de2db7b7498fc696ea7b244647cfe1
David Glick <dglick40@stuy.edu>
2023-05-23 14:00:48 -0400

    made hand class. need to work on getValue for hand.

--- Game.java
+++ Game.java
@@ -1,9 +1,9 @@
+import java.util.*;
+
 public class Game{
-  public static final int HIT = 1;
-  public static final int STAND = 2;
-  public boolean hasMoves = true;
   public int numOfHands = 1;
-  public ArrayList<Card> hand = new ArrayList<Card>();
+  public ArrayList<Hand> hands = new ArrayList<Hand>();
+  public ArrayList<Card> dealerHand = new ArrayList<Card>();
   public Scanner in = new Scanner(System.in);
   public void game(){
     if(numOfHands == 1){
@@ -12,21 +12,31 @@ public class Game{
     else{
       nextTurn();
     }
-    dealHand();
-    displayCards();
-    while(hasMoves){
-      move = getMove();
-      if(move.equals("hit")){
-        hand.add(getCard());
-        displayCards();
-        checkForMoves();
-      }
-      if(move.equals("stand")){
-        hasMoves = false;
+    for(int i = 0; i < hands.size(); i++){
+      Hand hand  = hands.get(i);
+      hand.dealHand();
+      hand.displayCards();
+      while(hand.hasMoves()){
+        String move = getMove();
+        if(move.equals("hit")){
+          hand.hit();
+        }
+        if(move.equals("stand")){
+          hand.stand();
+        }
       }
     }
-    dealerMove();
+    dealerhandMove();
     declareWinner();
+  }
+
+  public void dealerMove(){
+    dealHand(dealerHand);
+
+  }
+  public void nextTurn(){
+    System.out.println("Alright. Ready to play again?");
+    System.out.println("Let's go!");
   }
   public String getMove(){
     System.out.println("Please enter your move. Either hit or stand: ");
@@ -37,34 +47,5 @@ public class Game{
     System.out.println("Welcome to the casino!");
     System.out.println("Today you will be playing Blackjack.");
     System.out.println("The rules of blackjack are pretty simple: ");
-  }
-
-  public void dealHand(){
-    Card card1 = Deck.deal();
-    Card card2 = Deck.deal();
-    hand.add(card1);
-    hand.add(card2);
-  }
-
-  public void displayCards(){
-    System.out.println();
-    for(int i = 0; i < hand.size(); i++){
-      Card temp = hand.get(i);
-      if(temp.getValue() == 1){
-        System.out.print("ACE ");
-      }
-      else if(temp.getValue() == 11){
-        System.out.print("JACK ");
-      }
-      else if(temp.getValue() == 12){
-        System.out.print("QUEEN ");
-      }
-      else if(temp.getValue() == 13){
-        System.out.print("KING ");
-      }
-      else{
-        System.out.print(temp.getValue() + " ");
-      }
-    }
   }
 }
---
+++ Hand.java
@@ -0,0 +1,60 @@
+public class Hand{
+  private ArrayList<Card> hand = new ArrayList<Card>();
+
+  public hand(){
+
+  }
+  public void dealHand(){
+    Card card1 = Deck.deal();
+    Card card2 = Deck.deal();
+    hand.add(card1);
+    hand.add(card2);
+  }
+
+  public void displayCards(){
+    System.out.println();
+    for(int i = 0; i < hand.size(); i++){
+      Card temp = hand.get(i);
+      if(temp.getValue() == 1){
+        System.out.print("ACE ");
+      }
+      else if(temp.getValue() == 11){
+        System.out.print("JACK ");
+      }
+      else if(temp.getValue() == 12){
+        System.out.print("QUEEN ");
+      }
+      else if(temp.getValue() == 13){
+        System.out.print("KING ");
+      }
+      else{
+        System.out.print(temp.getValue() + " ");
+      }
+    }
+  }
+
+  public boolean hasMoves(){
+    if(getValue() >= 21){
+      return false;
+    }
+    return true;
+  }
+
+  public int getValue(){
+    int total = 0;
+    int numOfAces = 0;
+    for(int i = 0; i < hand.size(); i++){
+      Card temp = hand.get(i);
+      if(temp.getValue() != 1){
+        total+= temp.getValue();
+      }
+      else{
+        numOfAces++;
+      }
+    }
+    return total;
+  }
+
+
+
+}
